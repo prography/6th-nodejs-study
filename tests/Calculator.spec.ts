ca41774189a401b59ed589bb8446b7744f813ef9
geusan <dnay2k@gmail.com>
2020-04-14 03:03:13 +0900

    필요없는 파일 제거

--- tests/Calculator.spec.ts
+++
@@ -1,8 +0,0 @@
-import { Calculator } from '../src/Calculator';
-
-describe('test start', () => {
-  const calculator = new Calculator();
-  test('calculator.add', () => {
-    expect(calculator.add(1, 2)).toBe(3);
-  });
-});
