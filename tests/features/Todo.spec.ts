ab2acca50f9e5ec42c3ba0f84567e63f49064466
geusan <dnay2k@gmail.com>
2020-04-20 02:42:12 +0900

    테스트 코드 작성

--- tests/features/Todo.spec.ts
+++ tests/features/Todo.spec.ts
@@ -1,26 +1,16 @@
 import supertest from 'supertest';
 import { app } from '../../src/app';
-
-const assertItem = item => {
-  const expectedKeys = ['id', 'title', 'description', 'author'];
-  Object.keys(item).forEach(key => {
-    const idx = expectedKeys.indexOf(key);
-    if (idx > -1) {
-      expectedKeys.splice(idx, 1);
-    }
-  });
-  expect(expectedKeys).toBe([]);
-};
+import { PrismaClient } from '@prisma/client';
 
 describe('test Todo', () => {
   const client = supertest(app);
-
   test('test index todos', async () => {
     const response = await client.get('/todos');
     expect(response.status).toBe(200);
-    expect(Array.isArray(response.body)).toBe(true);
-    for (const item of response.body) {
-      assertItem(item);
-    }
+    const actual = await new PrismaClient().todo.findOne({
+      where: { id: Number(response.body.id) },
+    });
+    expect(actual.description).toBe(response.body.description);
+    expect(actual.title).toBe(response.body.title);
   });
 });
